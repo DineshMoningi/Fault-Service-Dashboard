b6a61b8c6a7df4b4603ddae12fef047ad0863a99
DineshMoningi <43263823+DineshMoningi@users.noreply.github.com>
2019-09-03 07:06:49 +0530

    Header changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,11 @@
 import React, { Component, Fragment } from "react";
-import { Header, Footer } from "../Layouts";
+import { Header } from "../Layouts";
 
 export default class extends Component {
   render() {
     return (
       <Fragment>
         <Header />
-        <Footer />
       </Fragment>
     );
   }
